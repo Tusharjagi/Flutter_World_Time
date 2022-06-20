9789b91b6f0fe3b9dafd0bdda1ee62bd0f097c31
Tushar jagi <tusharjagi@gmail.com>
2022-06-20 23:52:22 +0530

    date

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,8 +8,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
+
+    data =  ModalRoute.of(context)?.settings?.arguments as Map;
+    print(data);
+
     return Scaffold(
       body: SafeArea(
           child: Column(
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -10,15 +10,15 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String time = 'loading';
-
   void setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time!;
+    Navigator.pushReplacementNamed(context, '/home',arguments: {
+      'location' :instance.location,
+      'flag' : instance.flag,
+      'time' : instance.time,
     });
+    
   }
 
   @override
@@ -29,10 +29,10 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Padding(
-          padding: const EdgeInsets.all(50.0),
-        child: Text(time),
+          padding: EdgeInsets.all(50.0),
+        child: Text('loading'),
       ),
     );
   }
