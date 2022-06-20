59ddf3584bb0dee6657f7a3285ee1beb44b95f85
Tushar jagi <tusharjagi@gmail.com>
2022-06-20 20:19:45 +0530

    http

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,26 +9,7 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-  //  simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-        return 'tango';
-    });
 
-    //Simulate network request to get bio of the
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'or bhai kya haaal hain ';
-
-    });
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print("Hey tango");
-  }
 
   @override
   Widget build(BuildContext context) {
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,6 +11,20 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    Map data = jsonDecode(response.body);
+    print(data);
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
